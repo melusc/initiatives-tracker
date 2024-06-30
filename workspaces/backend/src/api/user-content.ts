0cff67fc1de534c0b26dd15df4b8de2ac5e6dbbd
Luca <61409445+melusc@users.noreply.github.com>
2024-06-30 16:17:21 +0200

    Render 404 on non-existant user-content

--- workspaces/backend/src/api/user-content.ts
+++ workspaces/backend/src/api/user-content.ts
@@ -7,16 +7,32 @@ import {imageOutDirectory, pdfOutDirectory} from '../paths.ts';
 // eslint-disable-next-line new-cap
 const router = Router();
 
-router.get('/pdf/:id', (request, response) => {
-	response.sendFile(request.params.id, {
-		root: fileURLToPath(pdfOutDirectory),
-	});
+router.get('/pdf/:id', (request, response, next) => {
+	response.sendFile(
+		request.params.id,
+		{
+			root: fileURLToPath(pdfOutDirectory),
+		},
+		error => {
+			if (error) {
+				next();
+			}
+		},
+	);
 });
 
-router.get('/image/:id', (request, response) => {
-	response.sendFile(request.params.id, {
-		root: fileURLToPath(imageOutDirectory),
-	});
+router.get('/image/:id', (request, response, next) => {
+	response.sendFile(
+		request.params.id,
+		{
+			root: fileURLToPath(imageOutDirectory),
+		},
+		error => {
+			if (error) {
+				next();
+			}
+		},
+	);
 });
 
 export {router as userContent};
